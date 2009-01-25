6a80ca509758107e055eaf418a44113fa381e8c0
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-25 14:09:33 +0000

    added test_data fixture for unit test
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@10394 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/CommunityTopicTest.php
+++ test/unit/model/CommunityTopicTest.php
@@ -5,3 +5,12 @@ include(dirname(__FILE__).'/../../bootstrap/database.php');
 
 $t = new lime_test(null, new lime_output_color());
 
+//------------------------------------------------------------
+// CommunityTopicPeer::isCreatable()
+// * コミュニティ設定
+//   * public_flag:     auth_commu_member
+//   * topic_authority: admin_only
+//------------------------------------------------------------
+
+// $t->diag('CommunityTopic::isCreatable()');
+// $t->isa_ok(CommunityTopicPeer::isCreatable(1, 1), 'community admin',
