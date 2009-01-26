67ff1fa5595a09c9928adafbe8c88bd622194738
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-26 04:56:18 +0000

    #3372:added unit test cases for the CommunityTopicComment::isDeletable() method
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@10411 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/CommunityTopicTest.php
+++ test/unit/model/CommunityTopicTest.php
@@ -3,7 +3,7 @@
 include(dirname(__FILE__).'/../../bootstrap/unit.php');
 include(dirname(__FILE__).'/../../bootstrap/database.php');
 
-$t = new lime_test(64, new lime_output_color());
+$t = new lime_test(84, new lime_output_color());
 
 //------------------------------------------------------------
 // Community::isCreatableCommunityTopic()
@@ -185,32 +185,39 @@ $t->diag('CommunityTopicComment::isDeletable()');
 // * public_flag:     auth_commu_member
 // * topic_authority: admin_only
 $t->diag('public_flag: auth_commu_member, topic_authority: admin_only');
-$community_a = CommunityPeer::retrieveByPk(1);
-$t->cmp_ok($community_a->isViewableCommunityTopic(1), '===', true, 'returns true for the community admin');
-$t->cmp_ok($community_a->isViewableCommunityTopic(3), '===', true, 'returns true for a community member');
-$t->cmp_ok($community_a->isViewableCommunityTopic(4), '===', false, 'returns false for a non-community member');
+$topic_comment_a = CommunityTopicCommentPeer::retrieveByPk(1);
+$t->cmp_ok($topic_comment_a->isDeletable(1), '===', true, 'returns true for the community admin');
+$t->cmp_ok($topic_comment_a->isDeletable(2), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($topic_comment_a->isDeletable(3), '===', false, 'returns false for a community member');
+$t->cmp_ok($topic_comment_a->isDeletable(4), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($topic_comment_a->isDeletable(5), '===', true, 'returns true for the community topic comment author');
 
 // * public_flag:     public
 // * topic_authority: admin_only
 $t->diag('public_flag: public, topic_authority: admin_only');
-$community_i = CommunityPeer::retrieveByPk(2);
-$t->cmp_ok($community_i->isViewableCommunityTopic(1), '===', true, 'returns true for the community admin');
-$t->cmp_ok($community_i->isViewableCommunityTopic(3), '===', true, 'returns true for a community member');
-$t->cmp_ok($community_i->isViewableCommunityTopic(4), '===', true, 'returns true for a non-community member');
+$topic_comment_i = CommunityTopicCommentPeer::retrieveByPk(2);
+$t->cmp_ok($topic_comment_i->isDeletable(1), '===', true, 'returns true for the community admin');
+$t->cmp_ok($topic_comment_i->isDeletable(2), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($topic_comment_i->isDeletable(3), '===', false, 'returns false for a community member');
+$t->cmp_ok($topic_comment_i->isDeletable(4), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($topic_comment_i->isDeletable(5), '===', true, 'returns true for the community topic comment author');
 
 // * public_flag:     auth_commu_member
 // * topic_authority: public
-$t->diag('public_flag: auth_commu_member, topic_authority: public');
-$community_u = CommunityPeer::retrieveByPk(3);
-$t->cmp_ok($community_u->isViewableCommunityTopic(1), '===', true, 'returns true for the community admin');
-$t->cmp_ok($community_u->isViewableCommunityTopic(3), '===', true, 'returns true for a community member');
-$t->cmp_ok($community_u->isViewableCommunityTopic(4), '===', false, 'returns false for a non-community member');
+$t->diag('public_flag: public, topic_authority: admin_only');
+$topic_comment_u = CommunityTopicCommentPeer::retrieveByPk(3);
+$t->cmp_ok($topic_comment_u->isDeletable(1), '===', true, 'returns true for the community admin');
+$t->cmp_ok($topic_comment_u->isDeletable(2), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($topic_comment_u->isDeletable(3), '===', false, 'returns false for a community member');
+$t->cmp_ok($topic_comment_u->isDeletable(4), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($topic_comment_u->isDeletable(5), '===', true, 'returns true for the community topic comment author');
 
 // * public_flag:     public
 // * topic_authority: public
 $t->diag('public_flag: public, topic_authority: public');
-$community_e = CommunityPeer::retrieveByPk(4);
-$t->cmp_ok($community_e->isViewableCommunityTopic(1), '===', true, 'returns true for the community admin');
-$t->cmp_ok($community_e->isViewableCommunityTopic(3), '===', true, 'returns true for a community member');
-$t->cmp_ok($community_e->isViewableCommunityTopic(4), '===', true, 'returns true for a non-community member');
-
+$topic_comment_e = CommunityTopicCommentPeer::retrieveByPk(4);
+$t->cmp_ok($topic_comment_e->isDeletable(1), '===', true, 'returns true for the community admin');
+$t->cmp_ok($topic_comment_e->isDeletable(2), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($topic_comment_e->isDeletable(3), '===', false, 'returns false for a community member');
+$t->cmp_ok($topic_comment_e->isDeletable(4), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($topic_comment_e->isDeletable(5), '===', true, 'returns true for the community topic comment author');
