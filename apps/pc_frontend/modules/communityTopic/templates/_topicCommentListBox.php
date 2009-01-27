3250a4c48537d2d584745723828b8c457093a7c8
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-27 08:14:13 +0000

    added support for truncate titles of the opCommunityTopicPlugin
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@10515 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/communityTopic/templates/_topicCommentListBox.php
+++ apps/pc_frontend/modules/communityTopic/templates/_topicCommentListBox.php
@@ -1,3 +1,5 @@
+<?php use_helper('opCommunityTopic') ?>
+
 <div class="dparts homeRecentList"><div class="parts">
 <div class="partsHeading"><h3><?php echo __('Recently Posted Community Topics') ?></h3></div>
 <div class="block">
@@ -5,7 +7,7 @@
 <?php if (count($communityTopic)): ?>
 <ul class="articleList">
 <?php foreach ($communityTopic as $topic): ?>
-<li><span class="date"><?php echo op_format_date($topic->getCreatedAt(), 'XShortDateJa') ?></span><?php echo link_to($topic->getName().' ('.$topic->countCommunityTopicComments().')', 'communityTopic_show', $topic).' ('.$topic->getCommunity()->getName().')' ?></li>
+<li><span class="date"><?php echo op_format_date($topic->getCreatedAt(), 'XShortDateJa') ?></span><?php echo link_to(op_community_topic_truncate($topic->getName(), 36, true).' ('.$topic->countCommunityTopicComments().')', 'communityTopic_show', $topic).' ('.$topic->getCommunity()->getName().')' ?></li>
 <?php endforeach; ?>
 </ul>
 <?php endif; ?>
