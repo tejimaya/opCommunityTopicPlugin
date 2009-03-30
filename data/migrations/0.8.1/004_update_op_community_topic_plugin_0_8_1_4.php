7b074eba62eabe84634baeaed3f4821fea90ed27
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-30 05:04:24 +0000

    #3672:added a migrate script for importing initialization data
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@11183 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ data/migrations/0.8.1/004_update_op_community_topic_plugin_0_8_1_4.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+class update_op_community_topic_plugin_0_8_1_4 extends opMigration
+{
+  public function up()
+  {
+    $eventNavigation = new Navigation();
+    $eventNavigation->setType('community');
+    $eventNavigation->setUri('communityEvent/listCommunity');
+    $eventNavigation->setSortOrder(6);
+    $eventNavigation->setCaption('Events', 'en');
+    $eventNavigation->setCaption('イベントリスト', 'ja_JP');
+
+    $gadget = new Gadget();
+    $gadget->setType('contents');
+    $gadget->setName('recentCommunityEventComment');
+    $gadget->setSortOrder(132);
+    $gadget->save();
+
+
+    $gadget = new Gadget();
+    $gadget->setType('mobileContents');
+    $gadget->setName('recentCommunityEventComment');
+    $gadget->setSortOrder(111);
+    $gadget->save();
+  }
+
+  public function down()
+  {
+  }
+}
