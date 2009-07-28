1283fe4f5068b5c7f180a5e725b3bb4b2e4ca77b
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-28 02:41:47 +0000

    fixed the unit test of opCommunityTopicPlugin with Zend_Acl
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@12430 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/CommunityTopicTest.php
+++ test/unit/model/CommunityTopicTest.php
@@ -3,14 +3,11 @@
 include(dirname(__FILE__).'/../../bootstrap/unit.php');
 include(dirname(__FILE__).'/../../bootstrap/database.php');
 
-$t = new lime_test(84, new lime_output_color());
+$t = new lime_test(64, new lime_output_color());
 
 $members = Doctrine::getTable('Member')->createQuery()->orderBy('id')->execute();
 $communities = Doctrine::getTable('Community')->createQuery()->orderBy('id')->execute();
 $topics = Doctrine::getTable('CommunityTopic')->createQuery()->orderBy('id')->execute();
-$topic_comments = Doctrine::getTable('CommunityTopicComment')->createQuery()->orderBy('id')->execute();
-$events = Doctrine::getTable('CommunityEvent')->createQuery()->orderBy('id')->execute();
-$event_comments = Doctrine::getTable('CommunityEventComment')->createQuery()->orderBy('id')->execute();
 
 function getAcl($object)
 {
@@ -20,18 +17,14 @@ function getAcl($object)
     case 'Community':
       return opCommunityTopicAclBuilder::buildCollection($object, $members);
     case 'CommunityTopic':
-      return opCommunityTopicAclBuilder::buildCollection($object->getCommunity(), $members);
-    case 'CommunityTopicComment':
-      return opCommunityTopicAclBuilder::buildCollection($object->getCommunityTopic()->getCommunity(), $members);
-    case 'CommunityEvent':
-      return opCommunityEventAclBuilder::buildCollection($object->getCommunity(), $members);
+      return opCommunityTopicAclBuilder::buildResource($object, $members);
   }
 }
 
 //------------------------------------------------------------
-// Community::isCreatableCommunityTopic()
+// Is the community creatable topics
 //------------------------------------------------------------
-$t->diag('Community::isCreatableCommunityTopic()');
+$t->diag('Is the community creatable a topic)');
 
 // * public_flag:     auth_commu_member
 // * topic_authority: admin_only
@@ -66,58 +59,58 @@ $t->cmp_ok($acl->isAllowed(3, null, 'add'), '===', true, 'returns true for a com
 $t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for a non-community member');
 
 //------------------------------------------------------------
-// CommunityTopic::isCreatableCommunityTopicComment()
+// Is the community topic creatable comments
 //------------------------------------------------------------
-$t->diag('CommunityTopic::isCreatableCommunityTopicComment()');
+$t->diag('Is the community topic creatable comments');
 
 // * public_flag:     auth_commu_member
 // * topic_authority: admin_only
 $t->diag('public_flag: auth_commu_member, topic_authority: admin_only');
 $acl = getAcl($topics[0]);
-$t->cmp_ok($acl->isAllowed(1, null, 'add'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'add'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'add'), '===', true, 'returns true for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($acl->isAllowed(1, null, 'addComment'), '===', true, 'returns true for the community admin');
+$t->cmp_ok($acl->isAllowed(2, null, 'addComment'), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($acl->isAllowed(3, null, 'addComment'), '===', true, 'returns true for a community member');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for a non-community member');
 $acl = getAcl($topics[4]);
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for the community topic author but a non-community member now');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for the community topic author but a non-community member now');
 
 // * public_flag:     public
 // * topic_authority: admin_only
 $t->diag('public_flag: public, topic_authority: admin_only');
 $acl = getAcl($topics[2]);
-$t->cmp_ok($acl->isAllowed(1, null, 'add'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'add'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'add'), '===', true, 'returns true for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($acl->isAllowed(1, null, 'addComment'), '===', true, 'returns true for the community admin');
+$t->cmp_ok($acl->isAllowed(2, null, 'addComment'), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($acl->isAllowed(3, null, 'addComment'), '===', true, 'returns true for a community member');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for a non-community member');
 $acl = getAcl($topics[5]);
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for the community topic author but a non-community member now');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for the community topic author but a non-community member now');
 
 // * public_flag:     auth_commu_member
 // * topic_authority: public
 $t->diag('public_flag: auth_commu_member, topic_authority: public');
 $acl = getAcl($topics[3]);
-$t->cmp_ok($acl->isAllowed(1, null, 'add'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'add'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'add'), '===', true, 'returns true for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($acl->isAllowed(1, null, 'addComment'), '===', true, 'returns true for the community admin');
+$t->cmp_ok($acl->isAllowed(2, null, 'addComment'), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($acl->isAllowed(3, null, 'addComment'), '===', true, 'returns true for a community member');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for a non-community member');
 $acl = getAcl($topics[6]);
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for the community topic author but a non-community member now');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for the community topic author but a non-community member now');
 
 // * public_flag:     public
 // * topic_authority: public
 $t->diag('public_flag: public, topic_authority: public');
 $acl = getAcl($topics[4]);
-$t->cmp_ok($acl->isAllowed(1, null, 'add'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'add'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'add'), '===', true, 'returns true for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for a non-community member');
+$t->cmp_ok($acl->isAllowed(1, null, 'addComment'), '===', true, 'returns true for the community admin');
+$t->cmp_ok($acl->isAllowed(2, null, 'addComment'), '===', true, 'returns true for the community topic author');
+$t->cmp_ok($acl->isAllowed(3, null, 'addComment'), '===', true, 'returns true for a community member');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for a non-community member');
 $acl = getAcl($topics[7]);
-$t->cmp_ok($acl->isAllowed(4, null, 'add'), '===', false, 'returns false for the community topic author but a non-community member now');
+$t->cmp_ok($acl->isAllowed(4, null, 'addComment'), '===', false, 'returns false for the community topic author but a non-community member now');
 
 //------------------------------------------------------------
-// CommunityTopic::isEditable()
+// Is the community topic editable
 //------------------------------------------------------------
-$t->diag('CommunityTopic::isEditable()');
+$t->diag('Is the community topic editable');
 
 // * public_flag:     auth_commu_member
 // * topic_authority: admin_only
@@ -164,9 +157,9 @@ $acl = getAcl($topics[7]);
 $t->cmp_ok($acl->isAllowed(4, null, 'edit'), '===', false, 'returns false for the community topic author but a non-community member now');
 
 //------------------------------------------------------------
-// Community::isViewableCommunityTopic()
+// Is the community viewable that topics
 //------------------------------------------------------------
-$t->diag('Community::isViewableCommunityTopic()');
+$t->diag('Is the community viewable that topics');
 
 // * public_flag:     auth_commu_member
 // * topic_authority: admin_only
@@ -199,48 +192,3 @@ $acl = getAcl($communities[3]);
 $t->cmp_ok($acl->isAllowed(1, null, 'view'), '===', true, 'returns true for the community admin');
 $t->cmp_ok($acl->isAllowed(3, null, 'view'), '===', true, 'returns true for a community member');
 $t->cmp_ok($acl->isAllowed(4, null, 'view'), '===', true, 'returns true for a non-community member');
-
-//------------------------------------------------------------
-// CommunityTopicComment::isDeletable()
-//------------------------------------------------------------
-$t->diag('CommunityTopicComment::isDeletable()');
-
-// * public_flag:     auth_commu_member
-// * topic_authority: admin_only
-$t->diag('public_flag: auth_commu_member, topic_authority: admin_only');
-$acl = getAcl($topic_comments[0]);
-$t->cmp_ok($acl->isAllowed(1, null, 'delete'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'delete'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'delete'), '===', false, 'returns false for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'delete'), '===', false, 'returns false for a non-community member');
-$t->cmp_ok($acl->isAllowed(5, null, 'delete'), '===', true, 'returns true for the community topic comment author');
-
-// * public_flag:     public
-// * topic_authority: admin_only
-$t->diag('public_flag: public, topic_authority: admin_only');
-$acl = getAcl($topic_comments[1]);
-$t->cmp_ok($acl->isAllowed(1, null, 'delete'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'delete'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'delete'), '===', false, 'returns false for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'delete'), '===', false, 'returns false for a non-community member');
-$t->cmp_ok($acl->isAllowed(5, null, 'delete'), '===', true, 'returns true for the community topic comment author');
-
-// * public_flag:     auth_commu_member
-// * topic_authority: public
-$t->diag('public_flag: public, topic_authority: admin_only');
-$acl = getAcl($topic_comments[2]);
-$t->cmp_ok($acl->isAllowed(1, null, 'delete'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'delete'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'delete'), '===', false, 'returns false for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'delete'), '===', false, 'returns false for a non-community member');
-$t->cmp_ok($acl->isAllowed(5, null, 'delete'), '===', true, 'returns true for the community topic comment author');
-
-// * public_flag:     public
-// * topic_authority: public
-$t->diag('public_flag: public, topic_authority: public');
-$acl = getAcl($topic_comments[3]);
-$t->cmp_ok($acl->isAllowed(1, null, 'delete'), '===', true, 'returns true for the community admin');
-$t->cmp_ok($acl->isAllowed(2, null, 'delete'), '===', true, 'returns true for the community topic author');
-$t->cmp_ok($acl->isAllowed(3, null, 'delete'), '===', false, 'returns false for a community member');
-$t->cmp_ok($acl->isAllowed(4, null, 'delete'), '===', false, 'returns false for a non-community member');
-$t->cmp_ok($acl->isAllowed(5, null, 'delete'), '===', true, 'returns true for the community topic comment author');
