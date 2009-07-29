2c3d72bc96fbec842fbc71ef78f751129aa80a8e
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-29 05:13:24 +0000

    #4185 add a japanese translation of a message of event members
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@12443 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/communityEvent/templates/memberListError.php
+++ apps/pc_frontend/modules/communityEvent/templates/memberListError.php
@@ -1,4 +1,4 @@
-<?php op_include_box('noMembers', __('Nobody joins this event'), array('title' => __('Error'))) ?>
+<?php op_include_box('noMembers', __('Nobody joins this event.'), array('title' => __('Event Members'))) ?>
 
 <?php use_helper('Javascript') ?>
 <?php op_include_line('backLink', link_to_function(__('Back to previous page'), 'history.back()')) ?>
