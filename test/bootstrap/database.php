5d59e8f058de86e2a2d883c8a848b76dbae7bae8
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2010-02-25 01:31:03 +0000

    fixed functional test runnable
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/branches/fuka-open-community@13347 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/bootstrap/database.php
+++ test/bootstrap/database.php
@@ -8,9 +8,18 @@
  * file and the NOTICE file that were distributed with this source code.
  */
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
+$_app = 'pc_frontend';
+$_env = 'test';
+
+$configuration = ProjectConfiguration::getApplicationConfiguration($_app, $_env, true);
 new sfDatabaseManager($configuration);
 
-$task = new sfDoctrineDataLoadTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run();
-$task->run(dirname(__FILE__).'/../fixtures');
+$task = new sfDoctrineBuildTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->setConfiguration($configuration);
+$task->run(array(), array(
+  'no-confirmation' => true,
+  'db'              => true,
+  'and-load'        => dirname(__FILE__).'/../fixtures',
+  'application'     => $_app,
+  'env'             => $_env,
+));
