c8b0ad1867e5c296b369c44ee2af27ceb3c077d1
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2010-07-14 19:50:09 +0000

    (refs #158)add migrate source
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@13586 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ data/migrations/1.0.0/005_update_op_community_topic_plugin_1_0_0_1.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+class update_op_community_topic_plugin_1_0_0_1 extends opMigration
+{
+  public function up()
+  {
+    $conn = Doctrine_Manager::connection();
+
+    $conn->export->createTable('community_topic_image',
+      array(
+        'id' => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+        'post_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'file_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'number' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+    ));
+    $conn->export->createIndex('community_topic_image', 'id_number', array(
+      'fields' => array('id', 'number'),
+      'type'   => 'unique',
+    ));
+
+    $conn->export->createTable('community_topic_comment_image',
+      array(
+        'id' => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+        'post_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'file_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'number' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+    ));
+    $conn->export->createIndex('community_topic_comment_image', 'id_number', array(
+      'fields' => array('id', 'number'),
+      'type'   => 'unique',
+    ));
+
+    $conn->export->createTable('community_event_image',
+      array(
+        'id' => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+        'post_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'file_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'number' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+    ));
+    $conn->export->createIndex('community_event_image', 'id_number', array(
+      'fields' => array('id', 'number'),
+      'type'   => 'unique',
+    ));
+
+    $conn->export->createTable('community_event_comment_image',
+      array(
+        'id' => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+        'post_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'file_id' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+        'number' => array('type' => 'integer', 'notnull' => '1', 'length' => '4'),
+    ));
+    $conn->export->createIndex('community_event_comment_image', 'id_number', array(
+      'fields' => array('id', 'number'),
+      'type'   => 'unique',
+    ));
+  }
+
+  public function down()
+  {
+    $this->dropTable('community_topic_image');
+    $this->dropTable('community_topic_comment_image');
+    $this->dropTable('community_event_image');
+    $this->dropTable('community_event_comment_image');
+  }
+}
