8c265a7793d823d3282e28249610ce819e5e16f9
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2010-07-16 14:45:29 +0000

    使用していないフォームクラスの記述を削除 (refs #158)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opCommunityTopicPlugin/trunk@13605 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/form/doctrine/PluginCommunityTopicCommentImageForm.class.php
+++ lib/form/doctrine/PluginCommunityTopicCommentImageForm.class.php
@@ -10,38 +10,4 @@
  */
 abstract class PluginCommunityTopicCommentImageForm extends BaseCommunityTopicCommentImageForm
 {
-  public function setup()
-  {
-    parent::setup();
-
-    unset($this['id']);
-    $this->useFields();
-
-    $key = 'photo';
-
-    $options = array(
-        'file_src'     => '',
-        'is_image'     => true,
-        'label'        => false,
-        'edit_mode'    => false,
-        );
-
-    $this->setWidget($key, new sfWidgetFormInputFileEditable($options, array('size' => 40)));
-    $this->setValidator($key, new opValidatorImageFile(array('required' => false)));
-  }
-
-  public function updateObject($values = null)
-  {
-    if ($values['photo'] instanceof sfValidatedFile)
-    {
-      $file = new File();
-      $file->setFromValidatedFile($values['photo']);
-
-      $this->getObject()->setFile($file);
-    }
-    else
-    {
-      $this->getObject()->setFile(null);
-    }
-  }
 }
