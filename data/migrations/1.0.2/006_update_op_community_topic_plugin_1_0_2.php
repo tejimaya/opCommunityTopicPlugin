7cc6441752d01fb71b706b52849883eacc0f48b0
watanabe <watanabe@tejimaya.com>
2011-09-26 19:17:04 +0900

    (fixes #2433) fixed migrate script for revision 006

--- data/migrations/1.0.2/006_update_op_community_topic_plugin_1_0_2.php
+++ data/migrations/1.0.2/006_update_op_community_topic_plugin_1_0_2.php
@@ -12,6 +12,7 @@ class update_op_community_topic_plugin_1_0_2 extends opMigration
 {
   public function up()
   {
+    $import = Doctrine_Manager::connection()->import;
     $export = Doctrine_Manager::connection()->export;
 
     $tableList = array(
@@ -23,31 +24,39 @@ class update_op_community_topic_plugin_1_0_2 extends opMigration
 
     foreach ($tableList as $table)
     {
-      $export->createIndex($table.'_image', 'post_id_idx', array(
-        'fields' => array('post_id')
-      ));
+      $tableName = $table.'_image';
+      $indexes = $import->listTableIndexes($tableName);
+      if (!in_array('post_id_idx', $indexes))
+      {
+        $export->createIndex($tableName, 'post_id_idx', array(
+          'fields' => array('post_id')
+        ));
 
-      $export->createIndex($table.'_image', 'file_id_idx', array(
-        'fields' => array('file_id')
-      ));
+        $definition = array(
+          'name'          => $tableName.'_file_id_file_id',
+          'local'         => 'file_id',
+          'foreign'       => 'id',
+          'foreignTable'  => 'file',
+          'onDelete'      => 'CASCADE'
+        );
+        $export->createForeignKey($tableName, $definition);
+      }
 
-      $definition = array(
-                  'name'          => $table.'_image_file_id_file_id',
-                  'local'         => 'file_id',
-                  'foreign'       => 'id',
-                  'foreignTable'  => 'file',
-                  'onDelete'      => 'CASCADE'
-      );
-      $export->createForeignKey($table.'_image', $definition);
+      if (!in_array('file_id_idx', $indexes))
+      {
+        $export->createIndex($tableName, 'file_id_idx', array(
+          'fields' => array('file_id')
+        ));
 
-      $definition = array(
-                  'name'          => $table.'_image_post_id_'.$table.'_id',
-                  'local'         => 'post_id',
-                  'foreign'       => 'id',
-                  'foreignTable'  => $table,
-                  'onDelete'      => 'CASCADE'
-      );
-      $export->createForeignKey($table.'_image', $definition);
+        $definition = array(
+          'name'          => $tableName.'_post_id_'.$table.'_id',
+          'local'         => 'post_id',
+          'foreign'       => 'id',
+          'foreignTable'  => $table,
+          'onDelete'      => 'CASCADE'
+        );
+        $export->createForeignKey($tableName, $definition);
+      } 
     }
   }
 }
