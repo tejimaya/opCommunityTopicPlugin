a57e54ef93122160e4f70c11895c645a9bdbf5da
watanabe <watanabe@tejimaya.com>
2011-09-27 14:20:58 +0900

    (refs #2433) swap definition in migrate script

--- data/migrations/1.0.2/006_update_op_community_topic_plugin_1_0_2.php
+++ data/migrations/1.0.2/006_update_op_community_topic_plugin_1_0_2.php
@@ -33,10 +33,10 @@ class update_op_community_topic_plugin_1_0_2 extends opMigration
         ));
 
         $definition = array(
-          'name'          => $tableName.'_file_id_file_id',
-          'local'         => 'file_id',
+          'name'          => $tableName.'_post_id_'.$table.'_id',
+          'local'         => 'post_id',
           'foreign'       => 'id',
-          'foreignTable'  => 'file',
+          'foreignTable'  => $table,
           'onDelete'      => 'CASCADE'
         );
         $export->createForeignKey($tableName, $definition);
@@ -49,10 +49,10 @@ class update_op_community_topic_plugin_1_0_2 extends opMigration
         ));
 
         $definition = array(
-          'name'          => $tableName.'_post_id_'.$table.'_id',
-          'local'         => 'post_id',
+          'name'          => $tableName.'_file_id_file_id',
+          'local'         => 'file_id',
           'foreign'       => 'id',
-          'foreignTable'  => $table,
+          'foreignTable'  => 'file',
           'onDelete'      => 'CASCADE'
         );
         $export->createForeignKey($tableName, $definition);
