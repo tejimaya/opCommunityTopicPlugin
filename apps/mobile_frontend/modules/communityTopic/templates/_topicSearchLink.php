d41568a7547383e5f6b1e78687893a8d9b1de212
watanabe <watanabe@openpne.jp>
2012-02-09 13:50:17 +0900

    (fixes #2514) replace ja to en for i18n in mobile page

--- apps/mobile_frontend/modules/communityTopic/templates/_topicSearchLink.php
+++ apps/mobile_frontend/modules/communityTopic/templates/_topicSearchLink.php
@@ -1,4 +1,4 @@
 <div align="right">
 <font color="<?php echo $op_color['core_color_20'] ?>">⇒</font>
-<?php echo link_to('ﾄﾋﾟｯｸ検索', 'communityTopic/search') ?>
+<?php echo link_to(__('Search %Community% Topics'), 'communityTopic/search') ?>
 </div>
