adff371e701c2040abeb41413accdca1bd7cdf98
Shunsuke Watanabe <watanabe@craftgear.net>
2012-09-23 13:09:54 +0900

    add a test case against checking privilege

--- test/functional/api/communityTopicPostTest.php
+++ test/functional/api/communityTopicPostTest.php
@@ -4,12 +4,7 @@ include dirname(__FILE__).'/../../bootstrap/functional.php';
 
 $t = new opTestFunctional(new sfBrowser());
 
-include dirname(__FILE__).'/../../bootstrap/database.php';
-
-$mailAddress = 'sns1@example.com';
-
-$t->login($mailAddress, 'password');
-$t->setCulture('en');
+//include dirname(__FILE__).'/../../bootstrap/database.php';
 
 $t->info('should be able to post a new topic');
 $name = 'テストタイトル';
@@ -31,3 +26,21 @@ $t->test()->ok($data['data']['member'], 'should have a member info');
 $t->test()->is($data['data']['name'], $name, 'should have the same name posted');
 $t->test()->is($data['data']['body'], $body, 'should have the same body posted');
 $t->test()->ok($data['data']['created_at'], 'should have the date posted');
+
+
+$t->info('members should NOT be able to post a new topic on communities with topic_authority "admin_only"');
+$name = '投稿できないタイトル';
+$body = '投稿できない本文';
+$json = $t->post('/topic/post.json',
+    array(
+      'apiKey'       => 'dummyApiKey2',
+      'community_id' => 1,
+      'name'         => $name,
+      'body'         => $body,
+    )
+  )->getResponse()->getContent()
+;
+$data = json_decode($json, true);
+$t->test()->is($data['status'], 'success', 'should return status code "success"');
+
+
