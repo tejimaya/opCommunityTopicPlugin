5d4179d8a88e8b1fdbf7c474cb93dce9002001fb
Shunsuke Watanabe <watanabe@craftgear.net>
2012-09-30 09:42:59 +0900

    add comment id to topic list link

--- apps/api/modules/communityTopic/templates/searchMiniSuccess.php
+++ apps/api/modules/communityTopic/templates/searchMiniSuccess.php
@@ -11,10 +11,12 @@ if (count($topics))
     $_topic = op_api_community_topic_mini($topic);
     $comments = $topic->getCommunityTopicComment();
     $_topic['latest_comment'] = '';
+    $_topic['latest_comment_id'] = '';
     if(count($comments))
     {
       $latest_comment = op_api_community_topic_comment($comments->getLast());
       $_topic['latest_comment'] = $latest_comment['body'];
+      $_topic['latest_comment_id'] = $latest_comment['id'];
     }
   
     $data[] = $_topic;
