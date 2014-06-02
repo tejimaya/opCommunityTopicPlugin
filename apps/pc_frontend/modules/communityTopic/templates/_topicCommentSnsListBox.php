bb1f0c2444f49af83c34ab9629c1ddbd90740cc2
Kimura Youichi <kim.upsilon@bucyou.net>
2014-06-03 01:47:34 +0900

    add the topicCommentSnsListBox gadget to display all topics in the SNS. (fixes #4078, BP from #3610)

---
+++ apps/pc_frontend/modules/communityTopic/templates/_topicCommentSnsListBox.php
@@ -0,0 +1,21 @@
+<?php if (count($communityTopic)): ?>
+<?php slot('body') ?>
+<?php foreach ($communityTopic as $topic): ?>
+<li><span class="date"><?php echo op_format_date($topic->getUpdatedAt(), 'XShortDateJa') ?></span>
+<?php echo sprintf('%s (%s)',
+  link_to(sprintf('%s(%d)',
+    op_truncate($topic->getName(), 36),
+    $topic->getCommunityTopicComment()->count()
+  ), '@communityTopic_show?id='.$topic->getId()),
+  $topic->getCommunity()->getName()
+) ?></li>
+<?php endforeach ?>
+<?php end_slot() ?>
+<?php echo op_include_parts('topicRecentList', 'homeRecentList_'.$gadget->id, array(
+  'title' => 'SNS全体のコミュニティ最新書き込み',
+  'listBody' => get_slot('body'),
+  'moreInfo' => array(
+    link_to(__('More'), array('sf_route' => 'communityTopic_search_all')),
+  ),
+)) ?>
+<?php endif ?>
